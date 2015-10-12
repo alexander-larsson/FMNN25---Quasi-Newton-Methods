dd1ad6e86edf255c8a2d75bb4e1df5bb235a47db
Alexander Larsson <alexx.1992@gmail.com>
2015-10-12 21:57:32 +0200

    trying to figure out why good broyden doesnt work

--- QuasiNewton.py
+++ QuasiNewton.py
@@ -19,6 +19,7 @@ class QuasiNewton(OptimizationMethod):
         if self.problem.grad is None:
             gradient = self.get_gradient(self.problem.obj_func,x_k)
         else:
+            ## I dont account for this case in the code below
             gradient = [g(*x_k) for g in self.problem.grad]
         for _ in range(10000):
             if gradient_is_zero(gradient):
@@ -27,17 +28,29 @@ class QuasiNewton(OptimizationMethod):
             #s_k = la.cho_solve((L,True),gradient)
             s_k = np.dot(inv_hessian, gradient)
             alpha_k = searchMethod(x_k, s_k, cond)
-            x_k_old = x_k;
-            x_k = x_k - alpha_k*s_k
-            delta = (x_k - x_k_old)
-            gamma = (self.get_gradient(self.problem.obj_func,x_k_old)) - (self.get_gradient(self.problem.obj_func,x_k))
-            hessian = self.goodBroyden(delta,gamma,hessian)
-            gradient = self.get_gradient(self.problem.obj_func,x_k)
+            print("x_k",x_k)
+            print("alpha_k",alpha_k)
+            x_k_new = x_k - alpha_k*s_k
+            print("x_k_new",x_k_new)
+            gradient_new = self.get_gradient(self.problem.obj_func,x_k_new)
+            #delta = x_k_new - x_k
+            delta = (np.array(x_k_new) - np.array(x_k)).reshape(len(x_k), 1)
+            print("grad",gradient)
+            print("grad_new",gradient_new)
+            #gamma = gradient - gradient_new
+            gamma = (gradient_new - gradient).reshape(len(x_k), 1)
+            inv_hessian = self.next_inv_hessian(delta,gamma,inv_hessian)
+            gradient = gradient_new
+            x_k = x_k_new
         raise Exception("Newtons method did not converge")
 
-    def goodBroyden(self,delta,gamma,hessian):
-        u = delta - np.dot(hessian,gamma)
+    def next_inv_hessian(self,delta,gamma,inv_hessian):
+        # Good broyden
+        # Will make this work then move to other class
+        print("delta",delta)
+        print("gamma",gamma)
+        u = delta - np.dot(inv_hessian,gamma)
+        print("u",u)
         #a = 1/(u.dot(gamma))
         a = 1 / np.dot(np.transpose(u),gamma)
-        hessian = hessian + a * np.dot(u,np.transpose(u))
-        return hessian
+        return inv_hessian + a * np.dot(u,np.transpose(u))
