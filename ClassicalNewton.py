6548b556a91f24d50817f6d2fe70671990f3c57c
Alexander Larsson <alexx.1992@gmail.com>
2015-10-12 19:11:28 +0200

    Good broyden funkar inte

--- ClassicalNewton.py
+++ ClassicalNewton.py
@@ -14,6 +14,7 @@ class ClassicalNewton(OptimizationMethod):
             raise TypeError("search must be one of 'exact' or 'inexact'")
         return self.classic_newton_method(initial_guess, searchMethod, cond)
 
+
     def classic_newton_method(self, initial_guess, searchMethod, cond):
         def gradient_is_zero(gradient):
             epsilon = 0.00000001
@@ -35,12 +36,35 @@ class ClassicalNewton(OptimizationMethod):
             L = la.cholesky(hessian, lower=True)
             s_k = la.cho_solve((L,True),gradient)
             alpha_k = searchMethod(x_k, s_k, cond)
+            x_k = x_k - alpha_k*s_k
+            gradient = self.get_gradient(self.problem.obj_func,x_k)
+        raise Exception("Newtons method did not converge")
+
+    def classic_newton_method_new(self, initial_guess, searchMethod, cond):
+        # Good broyden
+        def gradient_is_zero(gradient):
+            epsilon = 0.00000001
+            return np.all(list(map(lambda x: np.abs(x) < epsilon,gradient)))
+
+        if initial_guess is None:
+            x_k = np.zeros(self.problem.obj_func.__code__.co_argcount)
+        else:
+            x_k = np.array(initial_guess)
+        hessian = self.get_hessian(self.problem.obj_func,x_k)
+        if self.problem.grad is None:
+            gradient = self.get_gradient(self.problem.obj_func,x_k)
+        else:
+            gradient = [g(*x_k) for g in self.problem.grad]
+        for _ in range(10000):
+            if gradient_is_zero(gradient):
+                return x_k
+            L = la.cholesky(hessian, lower=True)
+            s_k = la.cho_solve((L,True),gradient)
+            alpha_k = searchMethod(x_k, s_k, cond)
             x_k_old = x_k;
             x_k = x_k - alpha_k*s_k
-            x_k_new = x_k;
-            delta = x_k_new - x_k_old;
-            gamma = (self.get_gradient(self.problem.obj_func,x_k_old)) -(self.get_gradient(self.problem.obj_func,x_k_new))
-            #Kommentera bort raden nedan för att ej köra goodBroyden
+            delta = x_k - x_k_old;
+            gamma = (self.get_gradient(self.problem.obj_func,x_k_old)) -(self.get_gradient(self.problem.obj_func,x_k))
             hessian = self.goodBroyden(delta,gamma,hessian);
             gradient = self.get_gradient(self.problem.obj_func,x_k)
         raise Exception("Newtons method did not converge")
